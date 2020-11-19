13ccd54c449d3215f96161867d915f064d212e02
nasparo <nasparo@yandex.ru>
2020-11-19 19:40:44 +0300

    activity not found

--- app/src/main/java/com/example/todolist/screens/main/MainViewModel.java
+++ app/src/main/java/com/example/todolist/screens/main/MainViewModel.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class MainViewModel extends ViewModel {
     private LiveData<List<Note>> noteLiveData = App.getInstance().getNoteDao().getAllLiveData();
 
+
+
     public LiveData<List<Note>> getNoteLiveData() {
         return noteLiveData;
     }
